11a48da6239b5e28fd7b3ad64ce048e68df3a735
sreskala <sam.sagas@hotmail.com>
2020-10-13 16:00:37 -0400

    changed the size of the slideshow

--- slideshow.js
+++ slideshow.js
@@ -4,10 +4,10 @@ $(document)
 
         setInterval(function () {
             $('#slideshow > div:first')
-                .fadeOut(1000)
+                .fadeOut(2000)
                 .next()
-                .fadeIn(1000)
+                .fadeIn(2000)
                 .end()
                 .appendTo('#slideshow');
-        }, 3000);
+        }, 6000);
     });
